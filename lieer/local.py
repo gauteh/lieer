2f37f4a86a20bdb810520623169051234f35b896
Jakub Jindra <jindraj@users.noreply.github.com>
2019-06-11 09:43:04 +0200

    Update local.py
    fix typo delimter -> delimiter

--- lieer/local.py
+++ lieer/local.py
@@ -334,7 +334,7 @@ class Local:
     if f > 5:
       return fname[:f]
     else:
-      print ("'%s' does not contain valid maildir delimter, correct file name extension, or does not seem to have a valid GID, ignoring.")
+      print ("'%s' does not contain valid maildir delimiter, correct file name extension, or does not seem to have a valid GID, ignoring.")
       return None
 
   def __make_maildir_name__ (self, m, labels):
