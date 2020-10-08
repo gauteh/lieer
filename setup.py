07b7717d004ed870bc2e4a7d0328eabe1f7fe883
Gaute Hope <eg@gaute.vetsj.com>
2020-10-08 10:17:13 +0200

    notmuch: use notmuch2 bindings
    missing `get_directory` in bindings

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['oauth2client', 'google-api-python-client', 'tqdm', 'notmuch'],
+    install_requires=['oauth2client', 'google-api-python-client', 'tqdm', 'notmuch2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
