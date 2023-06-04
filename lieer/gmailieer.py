gauteh/lieer

commit f7c3f594ee4a04c8289e8431df9d2e5470affc56

add -v --verbose option for push & pull to show same changes as --dry-run

lieer/gmailieer.py
@@ -67,6 +67,9 @@ class Gmailieer:
     parser_pull.add_argument ('-d', '--dry-run', action='store_true',
         default = False, help = 'do not make any changes')
 
+    parser_pull.add_argument ('-v', '--verbose', action='store_true',
+        default = False, help = 'print list of changes')
+
     parser_pull.add_argument ('-f', '--force', action = 'store_true',
         default = False, help = 'Force a full synchronization to be performed')
 
@@ -85,6 +88,9 @@ class Gmailieer:
 
     parser_push.add_argument ('-d', '--dry-run', action='store_true',
         default = False, help = 'do not make any changes')
+
+    parser_push.add_argument ('-v', '--verbose', action='store_true',
+        default = False, help = 'print list of changes')
 
     parser_push.add_argument ('-f', '--force', action = 'store_true',
         default = False, help = 'Push even when there has been remote changes (might overwrite remote tag-changes)')
@@ -272,6 +278,7 @@ class Gmailieer:
         raise NotADirectoryError("error: %s is not a valid path!" % args.path)
 
     self.dry_run          = dry_run
+    self.verbose          = args.verbose
     self.HAS_TQDM         = (not args.no_progress)
     self.credentials_file = args.credentials
 

lieer/local.py
@@ -298,6 +298,7 @@ class Local:
     self.gmailieer = g
     self.wd = os.getcwd ()
     self.dry_run = g.dry_run
+    self.verbose = g.verbose
 
     # config and state files for local repository
     self.config_f = os.path.join (self.wd, '.gmailieer.json')
@@ -520,9 +521,9 @@ class Local:
     except LookupError:
         nmsg = None
 
-    if self.dry_run:
-      print ("(dry-run) deleting %s: %s." % (gid, fname))
-    else:
+    self.print_changes ("deleting %s: %s." % (gid, fname))
+    
+    if not self.dry_run:
       if nmsg is not None:
         db.remove(fname)
       os.unlink (fname)
@@ -621,8 +622,7 @@ class Local:
         if not os.path.exists (fname):
           raise Local.RepositoryException ("tried to update tags on non-existent file: %s" % fname)
 
-      else:
-        print ("(dry-run) tried to update tags on non-existent file: %s" % fname)
+      self.print_changes ("tried to update tags on non-existent file: %s" % fname)
 
     try:
       nmsg = db.get(fname)
@@ -630,9 +630,8 @@ class Local:
       nmsg = None
 
     if nmsg is None:
-      if self.dry_run:
-        print ("(dry-run) adding message: %s: %s, with tags: %s" % (gid, fname, str(labels)))
-      else:
+      self.print_changes ("adding message: %s: %s, with tags: %s" % (gid, fname, str(labels)))
+      if not self.dry_run:
         try:
           (nmsg, _) = db.add (fname, sync_flags = True)
         except notmuch2.FileNotEmailError:
@@ -667,12 +666,17 @@ class Local:
           nmsg.tags.to_maildir_flags()
           self.__update_cache__ (nmsg, (gid, fname))
 
-        else:
-          print ("(dry-run) changing tags on message: %s from: %s to: %s" % (gid, str(otags), str(labels)))
+        self.print_changes ("changing tags on message: %s from: %s to: %s" % (gid, str(otags), str(labels)))
 
         return True
       else:
         return False
+
+  def print_changes (self, changes):
+    if self.dry_run:
+      print ("(dry-run) " + changes)
+    elif self.verbose:
+      print(changes)
 
 
 

lieer/remote.py
@@ -123,6 +123,7 @@ class Remote:
     self.CLIENT_SECRET_FILE = g.credentials_file
     self.account = g.local.config.account
     self.dry_run = g.dry_run
+    self.verbose = g.verbose
 
     self.ignore_labels = self.gmailieer.local.config.ignore_remote_labels
 
@@ -565,8 +566,8 @@ class Remote:
           print ("update: %s: Trying to add both SPAM and INBOX, dropping INBOX (add: %s, rem: %s)" % (gid, add, rem))
           add.remove('INBOX')
 
+      self.print_changes ("gid: %s: add: %s, remove: %s" % (gid, str(add), str(rem)))
       if self.dry_run:
-        print ("(dry-run) gid: %s: add: %s, remove: %s" % (gid, str(add), str(rem)))
         return None
       else:
         return self.__push_tags__ (gid, add, rem)
@@ -738,4 +739,10 @@ class Remote:
       message['threadId'] = threadId
 
     return self.service.users().messages().send(userId = self.account, body = message).execute()
+
+  def print_changes (self, changes):
+    if self.dry_run:
+      print ("(dry-run) " + changes)
+    elif self.verbose:
+      print(changes)
 

commit 603933f15beef13c6caa45b99f3cbf4651d2f13f

Add verbose option to sync
Fixes f7c3f594ee4a04c8289e8431df9d2e5470affc56 which broke sync.

lieer/gmailieer.py
@@ -135,6 +135,9 @@ class Gmailieer:
     parser_sync.add_argument ('-d', '--dry-run', action='store_true',
         default = False, help = 'do not make any changes')
 
+    parser_sync.add_argument ('-v', '--verbose', action='store_true',
+        default = False, help = 'print list of changes')
+
     parser_sync.add_argument ('-f', '--force', action = 'store_true',
         default = False, help = 'Push even when there has been remote changes, and force a full remote-to-local synchronization')
 